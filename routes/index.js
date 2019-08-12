c70f6d463573e453ca2bd97fb93fd92655a7a7b0
Mohd Ashif <theashifkhan@gmail.com>
2019-08-12 19:17:11 +0530

    Password

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router  = express.Router();
 
 var User = require("../models/user");
+var Newuser = require("../models/alluser");
 var passport   = require("passport");
+
 
 
 router.use(function(req, res, next){
@@ -18,6 +20,19 @@ router.get("/register",function(req,res){
 router.post("/register",function(req,res){
     console.log(req.body.username);
     console.log(req.body.password);
+     var newusername = req.body.username;
+     var newpassword = req.body.password;
+     Newuser.create({
+          email : newusername,
+          password : newpassword        
+     },function(err,newuser){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(newuser);
+            }
+     });
+
     User.register(new User({username:req.body.username}),req.body.password,function(err,user){
             if(err) {
                 console.log(err);
@@ -44,7 +59,17 @@ router.get("/logout",function(req,res){
     req.flash("error","log You Out");
     res.redirect("/campground");
 });
-
+router.get("/alluser",function(req,res){
+    Newuser.find({},function(err,foundusers){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(foundusers);
+            res.render("campgrounds/alluser.ejs",{Newuser:foundusers});
+        }
+    });
+    
+});
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();
