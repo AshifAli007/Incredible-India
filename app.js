467af06d82c22aedb7fe4af9dce28be969bb6bca
Ashif <ashif@Jarvis.local>
2024-02-09 07:44:59 -0500

    error of port fixed

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.use(function(req, res, next){
 // app.listen(process.env.PORT,process.env.IP,function(){
 //     console.log("yelpcamp Server Has Been Started");
 //     });
-app.listen(3000,"127.0.0.1",function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("yelpcamp Server Has Been Started at 3000");
     });
